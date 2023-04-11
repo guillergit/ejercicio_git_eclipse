40aa6e0a5609925d5666688b5e9358dcd04e1ae4
Guille <guillermolopezcalleja@gmail.com>
2023-04-11 09:51:33 +0200

    Se añade otro mensaje inferior con Alumno1:

--- ClasePrincipal.java
+++ ClasePrincipal.java
@@ -5,6 +5,7 @@ public class ClasePrincipal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Listado de alumnos 1ºDAM");
+		System.out.println("Alumno1:");
 	}
 
 }
