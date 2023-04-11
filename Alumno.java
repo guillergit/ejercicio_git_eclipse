635f5c98108a1dd609563bb7ef0446eef10717c3
Guille <guillermolopezcalleja@gmail.com>
2023-04-11 09:57:56 +0200

    Se añade constructor con parametros en clase Alumno

--- Alumno.java
+++ Alumno.java
@@ -3,4 +3,9 @@ package Ejercicio_git_eclipse_guillermo;
 public class Alumno {
 	private String nombre, apellido, dni;
 	
+	public Alumno (String n, String a, String d){
+		nombre = n;
+		apellido = a;
+		dni = d;
+	}
 }
