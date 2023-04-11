be208e70b228125092cf86b4751bc0a98731690e
Guille <guillermolopezcalleja@gmail.com>
2023-04-11 09:48:18 +0200

    Se añade mensaje de Listado de alumnos 1ºDAM

---
+++ ClasePrincipal.java
@@ -0,0 +1,10 @@
+package Ejercicio_git_eclipse_guillermo;
+
+public class ClasePrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Listado de alumnos 1ºDAM");
+	}
+
+}
